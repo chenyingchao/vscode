ae5903c745776882f13dac6ee1b5c2b35213d862
chenyingchao <893088206@qq.com>
2022-03-14 21:57:52 +0800

    黑白滤镜1

--- 黑白滤镜/0.glsl
+++ 黑白滤镜/0.glsl
@@ -2,13 +2,45 @@
 // #iChannel1 "file://Users/chenyingchao/vscode/三格/for_test.jpeg"
 
 #iChannel0 "./拜年/cat.jpg" 
+#iChannel1 "./黑白滤镜/2拉开对比/100.png" 
+#iChannel2 "./黑白滤镜/3对比夸张/100.png" 
+#iChannel3 "./黑白滤镜/4慢快门/100.png" 
+
 #include "./bin/rect.glsl"
 #include "./bin/blend/blend.glsl"
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 
     vec2 uv = fragCoord/iResolution.xy;
-    vec4 currentColor = vec4(0.7333, 0.0706, 0.0706, 1.0);
-    currentColor = texture(iChannel0, uv);
-    fragColor = currentColor;
+    vec4 currentColor = texture(iChannel0, uv);
+
+    //找格子
+
+    float b = currentColor.b * 63.0;//0---63 （25.5）
+    vec2 index0;//（3行 1列）
+    index0.y = floor(floor(b) / 8.0); //(3行)
+    index0.x = floor(b) - (index0.y * 8.0); //(1列)
+    
+    vec2 index1;//（3行 2列）
+    index1.y = floor(ceil(b) / 8.0);  //3行
+    index1.x = ceil(b) - (index1.y * 8.0); //(2列)
+
+    //转换坐标 找到位置
+
+    vec2 texPos1;
+    texPos1.x = (index0.x * 0.125) + (63.0/512.0 * currentColor.r);
+    texPos1.y = (index0.y * 0.125) + (63.0/512.0 * currentColor.g);
+
+    vec2 texPos2;
+    texPos2.x = (index1.x * 0.125) + (63.0/512.0 * currentColor.r);
+    texPos2.y = (index1.y * 0.125) + (63.0/512.0 * currentColor.g);
+
+    //取色
+    vec4 newColor1 = texture(iChannel3, texPos1);
+    vec4 newColor2 = texture(iChannel3, texPos2);
+
+    vec4 newColor = mix(newColor1, newColor2, fract(b));
+     
+    float s = mod(iGlobalTime, 10.0)/10.0;
+    fragColor = mix(currentColor, vec4(newColor.rgb, currentColor.w), s);
 }
